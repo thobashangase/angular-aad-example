eb777c66f5b2c6c9b6a59551d882351b2a3c3c39
Thoba Shangase <shangasett@outlook.com>
2023-04-23 07:53:17 +0200

    Add routing to home and todos

--- src/app/pages/todos/todos.module.ts
+++ src/app/pages/todos/todos.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class TodosModule { }
